8cfd5cae0fd52a37c198c59ae13f099b9322a8c1
HippieJim <hippiejim2896@gmail.com>
2024-03-06 01:11:35 -0600

    Dice Roller

---
+++ rollD8.js
@@ -0,0 +1,17 @@
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const rollResult = document.getElementById("rollResult");
+    const rollDiceImg = document.getElementById("rollDiceImg");
+    const imgs = [];
+    const values = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 8) + 1;
+        values.push(value);
+        imgs.push(`<img src="images/d8/${value}.png">`);
+    }
+
+    rollResult.textContent = `Dice: ${values.join(', ')}`;
+    rollDiceImg.innerHTML = imgs.join('');
+}
